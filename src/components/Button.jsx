fcb4e427e905e5e7782e9e2356fe58c0638eb7f5
임유진 <lyj5721@sookmyung.ac.kr>
2025-08-23 02:47:25 +0900

    Feat: 기록 페이지 수정 기능 구현

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,11 @@
 import "./Button.css";
 
-const Button = ({ text, type }) => {
-  return <button className={`Button Button_${type}`}>{text}</button>;
+const Button = ({ text, type, onClick }) => {
+  return (
+    <button className={`Button Button_${type}`} onClick={onClick}>
+      {text}
+    </button>
+  );
 };
 
 export default Button;
--- src/pages/Record.jsx
+++ src/pages/Record.jsx
@@ -4,16 +4,20 @@ import BroadcastInfo from "../components/BroadcastInfo";
 import PlayRecord from "../components/PlayRecord";
 import TimestampList from "../components/TimestampList";
 
+import { useState } from "react";
+
 const Record = () => {
+  const [isEditMode, setIsEditMode] = useState(false);
+
   return (
     <div>
-      <Header></Header>
+      <Header />
       <div style={{ display: "flex", justifyContent: "flex-start" }}>
-        <StreamerRecord />
+        <StreamerRecord isEditMode={isEditMode} setIsEditMode={setIsEditMode} />
       </div>
-      <BroadcastInfo></BroadcastInfo>
-      <PlayRecord></PlayRecord>
-      <TimestampList></TimestampList>
+      <BroadcastInfo isEditMode={isEditMode} />
+      <PlayRecord isEditMode={isEditMode} />
+      <TimestampList isEditMode={isEditMode} />
     </div>
   );
 };
